dcf08b96c21c23c2e5283c341f0bd8427fcd18cb
seanyjeong <sean8320@naver.com>
2026-01-25 04:13:36 +0900

    chore: v1.9.3 - squeeze 보유 표시 수정

--- web/src/routes/settings/+page.svelte
+++ web/src/routes/settings/+page.svelte
@@ -105,7 +105,7 @@
 		<div class="info-list">
 			<div class="info-item">
 				<span class="info-label">버전</span>
-				<span class="info-value">1.9.2</span>
+				<span class="info-value">1.9.3</span>
 			</div>
 			<div class="info-item">
 				<span class="info-label">개발자</span>
